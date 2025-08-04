a6fed9e9cddfdd1726e9a87a6c44fab0eb3ac4c2
Wizard <cristiandamianvena@gmail.com>
2025-08-03 22:54:43 -0300

    fix: test passes

--- apps/example/src/app/app.controller.ts
+++ apps/example/src/app/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData(): string {
+  getData() {
     return this.appService.getData()
   }
 }
